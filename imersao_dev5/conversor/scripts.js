832f8e22da33329bd3aadb9b12db5d4c06deb091
marcotorquato <marcotorquato97@gmail.com>
2022-09-07 15:26:13 +0200

    challenge Mentalist Random

--- imersao_dev5/conversor/scripts.js
+++ imersao_dev5/conversor/scripts.js
@@ -2,15 +2,13 @@ function Converter() {
     let elementValue = document.querySelector("#value")
     let value = elementValue.value
     let valueConverterNum = parseFloat (value)
-
     let valueReal = valueConverterNum * 5
-
-
-
-    console.log(valueReal)
-
-    let elementConverted = document.querySelector('#valueConvert')
+    let elementConverted = document.querySelector('#valueConvert');
     let valueConverter = `The value convert is $${valueReal}`;
 
-    elementConverted.innerHTML = valueConverter
+     elementConverted.innerHTML = valueConverter;
+
 }
+
+
+       
---
+++ imersao_dev5/mentalista/style/script.js
@@ -0,0 +1,7 @@
+let numeroSecreto = 3
+
+function Kick() {
+    var kick = parseInt(document.querySelector('#value').value)
+
+    console.log(kick)
+}
