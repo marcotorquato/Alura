4d1b5d22af1cfeec827efdd4cdace086fd1cd442
marcotorquato <marcotorquato97@gmail.com>
2022-09-07 16:08:35 +0200

    mentalist game

--- imersao_dev5/mentalista/style/script.js
+++ imersao_dev5/mentalista/style/script.js
@@ -1,7 +1,22 @@
-let numeroSecreto = 3
+let secretNumber = 3
+
 
 function Kick() {
     var kick = parseInt(document.querySelector('#value').value)
+    
+        if (kick === secretNumber){
+            document.querySelector('#result').innerHTML = (`YOU KICKED THE NUMBER ${kick}, CONGRATULATIONS!!!`)
+        }
+        else if (kick < secretNumber){
+            document.querySelector('#result').innerHTML = (`YOU KICKED THE NUMBER ${kick}, NUMBER TOO LOW`)
 
-    console.log(kick)
+        }
+        else if (kick > secretNumber){
+            document.querySelector('#result').innerHTML = (`YOU KICKED THE NUMBER ${kick}, NUMBER TOO HIGH`)
+
+        }
+        else {
+            document.querySelector('#result').innerHTML = (`PLEASE, WRITE A NUMBER!`)            
+        }
+        
 }
